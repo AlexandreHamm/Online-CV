98dd18700ed695e667712fb3fc9ac905e4875c2c
ahamm-prequal2021 <a.hamm@codeur.online>
2021-05-12 16:53:38 +0200

    fix: pills content

--- scripts.js
+++ scripts.js
@@ -11,16 +11,3 @@ var x = window.matchMedia("(max-width: 1000px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
 
-window.onload = function() {
-// scroll tab fix
-var y = document.getElementById('ex1-pills-1');
-// Basically `element_to_scroll_to` just have to be a reference
-// to any DOM element present on the page
-// Then:
-y.onclick = function(){
-    return false;
-    }
-}
-
-// TEST SCROLL DOWN
-
